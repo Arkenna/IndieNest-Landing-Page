196d259809b5b1a4c752c2c990d13f7de4f5184f
JohnnyGZ41 <johnnyundertale41@outlook.com>
2025-10-16 15:12:37 -0500

    chore: add ngx-translate for internationalization support. - Integrate `@ngx-translate/core` and `@ngx-translate/http-loader` for i18n. - Configure default and fallback languages in app.config.ts. - Add placeholder translation files for English and Spanish. - Update package.json and package-lock.json with new dependencies. - inject translate service in app component and configure default language.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {FooterContent} from './shared/presentation/components/footer-content/footer-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('LandingIndieNest');
+  protected readonly title = signal('learning-center');
+  private translate: TranslateService;
+
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
 }
