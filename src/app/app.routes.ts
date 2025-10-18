9eecc54773aad5ba7930cf3f77bed0c44c6c9846
JohnnyGZ41 <johnnyundertale41@outlook.com>
2025-10-18 15:01:47 -0500

    update(shared): updated app routes and components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import {Home} from './shared/presentation/views/home/home';
 
-export const routes: Routes = [];
+
+const about = () => import('./shared/presentation/views/about/about').then(m => m.About);
+const goals =() => import('./shared/presentation/views/goals/goals').then(m => m.Goals);
+const reviews=()=>import('./shared/presentation/views/reviews/reviews').then(m => m.Reviews);
+const pageNotFound = () => import('./shared/presentation/views/page-not-found/page-not-found').then(m => m.PageNotFound);
+const baseTitle = 'IndieNest';
+export const routes: Routes = [
+  { path: 'home',     component:      Home,                         title: `${baseTitle} - Home`  },
+  { path: 'about',    loadComponent:  about,                        title: `${baseTitle} - About`  },
+  { path: 'goals',    loadComponent:  goals,                        title: `${baseTitle} - Goals`  },
+  { path: 'reviews',    loadComponent:  reviews,                        title: `${baseTitle} - Reviews`  },
+  { path: '',         redirectTo:     '/home', pathMatch: 'full'  },
+  { path: '**',       loadComponent:  pageNotFound,                 title: `${baseTitle} - Page Not Found`  },
+];
