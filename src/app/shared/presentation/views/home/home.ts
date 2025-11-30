e729ce7a2b46d2448104e24363017671c382fcd7
JohnnyGZ41 <johnnyundertale41@outlook.com>
2025-11-30 06:12:55 -0500

    About the product and about the team

--- src/app/shared/presentation/views/home/home.ts
+++ src/app/shared/presentation/views/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,13 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrl: './home.css'
 })
 export class Home {
+  private sanitizer = inject(DomSanitizer);
 
+  videoProductUrl: SafeResourceUrl;
+  videoTeamUrl: SafeResourceUrl;
+
+  constructor() {
+    this.videoProductUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/MflE0rbYbNc');
+    this.videoTeamUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/8rpiarII-nk');
+  }
 }
